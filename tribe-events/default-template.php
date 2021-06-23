c4eef5ddaebd8e7fae84ea5655e037a73e3d7aa1
Julianne Ragland <jbragland@gmail.com>
2021-06-23 13:11:35 -0500

    Style single event

--- tribe-events/default-template.php
+++ tribe-events/default-template.php
@@ -4,9 +4,8 @@ $context = Timber::get_context();
 
 if (is_singular()) {
     $timber_post = new TimberPost();
-    $context['title'] = $timber_post->title;
     $context['breadcrumbs'] = bcn_display(true);
-    $context['sidebar'] = true; // for .container and line length
+    $context['sidebar'] = false;
 }
 
 Timber::render('templates/events.twig', $context);
