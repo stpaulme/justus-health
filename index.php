52dcd5f9487d69cb1e63a6dccc320222fd6dc9cc
Julianne Ragland <jbragland@gmail.com>
2021-06-30 15:12:21 -0500

    Fix module fields on posts page

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ $context['post'] = $post;
 $context['title'] = $post->title;
 $context['posts'] = new Timber\PostQuery();
 
+// Below the fold
+$below_modules = get_field('below_modules');
+$context['below_modules'] = spm_add_data_to_modules($below_modules);
+
 $templates = array('index.twig');
 if (is_home()) {
     array_unshift($templates, 'front-page.twig', 'home.twig');
